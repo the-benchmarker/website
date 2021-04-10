cc839bca43b968b33c55d13910a9c86459d90ce3
VincentJonathan <vinjongm@gmail.com>
2021-04-10 09:27:37 +0700

    add DataTable

---
+++ src/components/DataTable.tsx
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import Select from "react-select";
+import DataTable, { IDataTableColumn } from "react-data-table-component";
+import { Benchmark } from "../api";
+
+const columns: IDataTableColumn<Benchmark>[] = [
+  {
+    name: "Language",
+    selector: "language",
+    sortable: true,
+  },
+  {
+    name: "Framework",
+    selector: ({ framework }) => framework.name,
+    cell: ({ framework }) => (
+      <div>
+        <a href={framework.link}>{framework.name}</a> ({framework.version})
+      </div>
+    ),
+    sortable: true,
+  },
+  {
+    name: "Speed (64)",
+    selector: "speed64",
+    sortable: true,
+  },
+  {
+    name: "Speed (256)",
+    selector: "speed256",
+    sortable: true,
+  },
+  {
+    name: "Speed (512)",
+    selector: "speed512",
+    sortable: true,
+  },
+];
+
+interface SelectOption {
+  label: string;
+  value: string;
+}
+
+interface Props {
+  benchmarks: Benchmark[];
+}
+
+function Table({ benchmarks }: Props) {
+  const [selectedLanguages, setSelectedLanguages] = useState<SelectOption[]>(
+    []
+  );
+
+  const onChange = (data: any) => {
+    setSelectedLanguages(data);
+  };
+
+  return (
+    <div>
+      <Select
+        isMulti
+        onChange={onChange}
+        options={[
+          ...new Set(benchmarks.map((b) => b.language)),
+        ].map((lang) => ({ value: lang, label: lang }))}
+      />
+
+      <DataTable
+        columns={columns}
+        data={
+          selectedLanguages.length
+            ? benchmarks.filter((b) =>
+                selectedLanguages.map((l) => l.value).includes(b.language)
+              )
+            : benchmarks
+        }
+      />
+    </div>
+  );
+}
+
+export default Table;
