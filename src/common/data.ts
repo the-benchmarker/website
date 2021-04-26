7afef100ff9e837ec6837bbe193c9f3fd4e9ed89
VincentJonathan <vinjongm@gmail.com>
2021-04-26 16:13:53 +0700

    add format for data table

--- src/common/data.ts
+++ src/common/data.ts
@@ -1,9 +1,11 @@
 import { MetricTypes } from "../api";
+import { formatLatency, formatThousandSeparated } from "./formatter";
 
 export type ComparedMetric = {
   key: MetricTypes;
   title: string;
   longTitle?: string;
+  format?: (...args: any[]) => string | number;
 };
 
 export const CONCURRENCIES = [64, 256, 512] as const;
@@ -14,42 +16,51 @@ export const COMPARED_METRICS: ComparedMetric[] = [
     key: "totalRequests",
     title: "Total Requests",
     longTitle: "Total Request in 15 seconds",
+    format: formatThousandSeparated,
   },
   {
     key: "percentile50",
     title: "P50 Latency",
     longTitle: "50th Percentile Latency (ms)",
+    format: formatLatency,
   },
   {
     key: "percentile75",
     title: "P75 Latency",
     longTitle: "75th Percentile Latency (ms)",
+    format: formatLatency,
   },
   {
     key: "percentile90",
     title: "P90 Latency",
     longTitle: "90th Percentile Latency (ms)",
+    format: formatLatency,
   },
   {
     key: "percentile99",
     title: "P99 Latency",
     longTitle: "99th Percentile Latency (ms)",
+    format: formatLatency,
   },
   {
     key: "percentile99999",
     title: "P99.999 Latency",
     longTitle: "99.999th Percentile Latency (ms)",
+    format: formatLatency,
   },
   {
     key: "averageLatency",
     title: "Average Latency (ms)",
+    format: formatLatency,
   },
   {
     key: "minimumLatency",
     title: "Minimum Latency (ms)",
+    format: formatLatency,
   },
   {
     key: "maximumLatency",
     title: "Maximum Latency (ms)",
+    format: formatLatency,
   },
 ];
--- src/common/index.ts
+++ src/common/index.ts
@@ -1,2 +1,3 @@
 export * from "./interfaces";
 export * from "./data";
+export * from "./formatter";
